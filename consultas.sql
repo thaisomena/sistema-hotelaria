d0a3cb1fd40417340148586905fa8f71eb6155ef
Thais Omena <thaisomena136@gmail.com>
2026-03-30 09:57:09 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -7,3 +7,16 @@ JOIN Hospede h ON r.cpf_hospede = h.cpf_hospede;
 SELECT p.id_pagamento, p.valorTotal, r.id_reserva
 FROM Pagamento p
 JOIN Reserva r ON p.id_reserva = r.id_reserva;
+
+-- Ver reservas com hóspede e pagamento
+SELECT r.id_reserva, h.nome, p.valorTotal
+FROM Reserva r
+JOIN Hospede h ON r.cpf_hospede = h.cpf_hospede
+JOIN Pagamento p ON p.id_reserva = r.id_reserva;
+
+-- Reservas ordenadas por data de entrada
+SELECT r.id_reserva, h.nome, r.dataEntrada
+FROM Reserva r
+JOIN Hospede h ON r.cpf_hospede = h.cpf_hospede
+ORDER BY r.dataEntrada;
+
